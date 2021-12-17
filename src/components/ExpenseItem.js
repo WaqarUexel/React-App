9eb9cb9b155e59dbb352d6ccb81236b463b031e7
unknown <waqar@uexel.com>
2021-12-17 11:39:11 +0500

    3rd commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+import { Upload } from 'antd';
 
 function ExpenseItem(props) {
+  let title=props.title;
+  const clickHandler=()=>{
+    title='Updated';
+    console.log(title);
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -10,6 +16,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>title change</button>;
     </Card>
   );
 }
